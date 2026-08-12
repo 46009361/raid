a2556a1350757d4a97c480837e3c15b7c70a16e5
46009361 <165697051+46009361@users.noreply.github.com>
2026-08-12 01:20:01 -0700

    chore: create working product

--- form.js
+++ form.js
@@ -1,5 +1,41 @@
-const testing = () => {
+"use strict";
+const imgForm = document.querySelector("#imgForm");
+imgForm.lang.placeholder = navigator.languages;
+const getImg = () => {
     event.preventDefault();
-    alert("testing");
+    const i = imgForm;
+    const lang = i.lang;
+    const req = new Request(
+        `https://corsproxy.io/?${
+            new URLSearchParams({
+                "url": `https://share.redd.it/preview/user/${
+                    i.user.value
+                }/achievement/${i.trophy.value}${
+                    i.show.checked ? "?show-user-info=true" : ""
+                }`,
+                "key": 46009361,
+                // cache busting
+                "cb": Math.floor(Math.random()*1e6)
+            })
+        }`,
+        {
+            method: "GET",
+            headers: {
+                "Accept-Language": lang.value || lang.placeholder
+            }
+        }
+    );
+    let src;
+    fetch(req)
+    .then((res) => res.blob())
+    .then((blob) => {
+        src = URL.createObjectURL(blob);
+        const earned = document.querySelector("#earned");
+        earned.classList.remove("hidden");
+        earned.addEventListener("load", URL.revokeObjectURL.bind(src), {
+            once: true
+        });
+        earned.src = src;
+    });
 }
-document.querySelector("#getImg").addEventListener("submit", testing);
+imgForm.addEventListener("submit", getImg);
